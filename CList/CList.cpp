fe319b5f78ff6ec89e6fa315ebc25d58b7cd887f
NEWhyf <tigger.Feng@gmail.com>
2013-04-17 00:03:40 +0800

    Clist

---
+++ CList/CList.cpp
@@ -0,0 +1,50 @@
+﻿#include "CList.h"
+
+template<class T>
+CList<T>::~CList()
+{
+     if(NULL != m_Head)
+     {
+         delete[] m_Head;
+         m_Head = NULL;
+     }
+     m_size = 0;
+     m_length = MIX_LENGTH;
+}
+
+template<class T>
+int CList<T>::Add(T& point)
+{
+    // 为空的队列分配内存
+    if(NULL == m_Head)       
+    {
+       m_Head = new T[m_length];
+       if(NULL == m_Head)
+       {
+           cout<<"ERROR: not enough memeory for new!"<<endl;
+           return -1;
+       }
+    }
+    else if( m_size >= m_length )       //队列空间不足，扩充队列
+    {
+       int iOldLength = m_length;
+       
+       m_length = ((m_length >= MAX_LENGTH) ? MAX_LENGTH : 2*m_length );
+       
+       T* tmpHead = new T[m_length];
+       if(NULL == tmpHead)
+       {
+           m_length = iOldLength;
+           cout<<"ERROR: not enough memeory for new!"<<endl;
+           return -1;
+       }
+
+       memcpy(tmpHead, m_Head, sizeof(T)*m_size);
+       delete[] m_Head;
+       m_Head = tmpHead;
+    }
+    m_Head[m_size] = point;
+    m_size++;
+
+    return 0;
+}
---
+++ CList/CList.h
@@ -0,0 +1,30 @@
+﻿#ifndef CLIST_H_
+#define CLIST_H_
+
+#include<iostream>
+using namespace std;
+
+#define MIX_LENGTH 32
+#define MAX_LENGTH 1024
+
+template<class T>
+
+class CList
+{
+
+private:
+    int m_size;
+    int m_length;
+    T* m_Node;
+    T* m_Head;
+public:
+    CList():m_size(0),m_length(MAX_LENGTH),m_Node(NULL)
+    {
+    }
+    ~CList();
+    int Add(T&);
+    void Erase();
+    int size(){ return m_size; }
+};
+
+#endif
